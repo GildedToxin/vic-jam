7f4a049357feac5a4f60f8df1d49e6c7a7b0fd53
Toxin4426 <Toxin4426@gmail.com>
2026-03-27 01:53:47 -0400

    Camera and basic interaction

--- TimeGame/Assets/Scripts/PlayerController.cs
+++ TimeGame/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,81 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 10f;
+
+    [Header("Jumping & Gravity")]
+    public float gravity = -9.81f;
+    public float jumpHeight = 1.5f;
+
+    [Header("References")]
+    public Transform cameraTransform;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+
+    private Vector2 moveInput;
+    private bool jumpPressed;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+
+        if (cameraTransform == null)
+            cameraTransform = Camera.main.transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+        HandleGravity();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            jumpPressed = true;
+    }
+
+    void HandleMovement()
+    {
+        bool isGrounded = controller.isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        Vector3 inputDir = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+
+        if (inputDir.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(inputDir.x, inputDir.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            float angle = Mathf.LerpAngle(transform.eulerAngles.y, targetAngle, rotationSpeed * Time.deltaTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+        }
+
+        if (jumpPressed && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            jumpPressed = false;
+        }
+    }
+
+    void HandleGravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
---
+++ TimeGame/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float interactDistance = 3f;
+    public LayerMask interactLayer;
+    public Transform cameraTransform;
+
+   [SerializeField] private Interactable currentInteractable;
+
+    void Update()
+    {
+        CheckForInteractable();
+    }
+
+    void CheckForInteractable()
+    {
+        Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactDistance, interactLayer))
+        {
+            Interactable interactable = hit.collider.GetComponent<Interactable>();
+
+            if (interactable != null)
+            {
+                if (currentInteractable != interactable)
+                {
+                    currentInteractable = interactable;
+                    Debug.Log("Looking at: " + interactable.name);
+                }
+                return;
+            }
+        }
+
+        currentInteractable = null;
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+
+        if (currentInteractable != null)
+        {
+            currentInteractable.Interact();
+        }
+    }
+}
