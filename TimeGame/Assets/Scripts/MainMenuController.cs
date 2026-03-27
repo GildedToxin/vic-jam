a3e9b9f140595ddccb730e8e5816667488a552a5
Toxin4426 <Toxin4426@gmail.com>
2026-03-27 00:29:57 -0400

    The stuff

---
+++ TimeGame/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+}
