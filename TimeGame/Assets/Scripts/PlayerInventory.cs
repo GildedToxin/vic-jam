f0f705153e57d655d94e074cefc2b67678c3fe07
PranavAlla <Pranavalla@gmail.com>
2026-03-28 15:57:54 -0400

    Saving work on inventory

--- TimeGame/Assets/Scripts/PlayerInventory.cs
+++ TimeGame/Assets/Scripts/PlayerInventory.cs
@@ -6,9 +6,13 @@ using UnityEngine.InputSystem;
 public class PlayerInventory : MonoBehaviour
 {
     public List<InventoryItem> items = new List<InventoryItem>();
+    public List<GameObject> itemPrefabs = new List<GameObject>();
     public GameObject InventoryUI;
     [HideInInspector] public bool isInventoryOpen = false;
     [SerializeField] private InventoryItemRotation inventoryItemRotation;
+    [SerializeField] private List<GameObject> inventorySlots = new List<GameObject>();
+    private int itemCount = 0;
+    [SerializeField] private GameObject InventoryObjectRotationPoint;
 
     public void OnInventory(InputAction.CallbackContext context)
     {
@@ -23,6 +27,16 @@ public class PlayerInventory : MonoBehaviour
     public void AddItem(InventoryItem inventoryItem)
     {
         items.Add(inventoryItem);
+        // Instantiate the corresponding prefab in the inventory UI set the prefab using the index of the prefab and the itemCount number
+        GameObject itemPrefab = itemPrefabs[(int)inventoryItem - 1];
+        GameObject itemInstance = Instantiate(itemPrefab, inventorySlots[itemCount].transform);
+
+        // instantiate an itemPrefab as a child of InventoryObjectRotationPoint
+        GameObject itemInstanceRotation = Instantiate(itemPrefab, InventoryObjectRotationPoint.transform);
+
+        
+        itemInstance.transform.localPosition = Vector3.zero;
+        itemCount++;
     }
     public void RemoveItem(InventoryItem inventoryItem)
     {
