dd629781a0745e14e80a04c66d921127d80e91ea
Toxin4426 <Toxin4426@gmail.com>
2026-03-29 14:49:40 -0400

    update

--- TimeGame/Assets/Scripts/Ferryman.cs
+++ TimeGame/Assets/Scripts/Ferryman.cs
@@ -6,6 +6,9 @@ public class Ferryman : Lock
     public GameObject closeJaw;
     public GameObject coin;
 
+    public GameObject fire;
+    public GameObject blue;
+
     
     public override void Interact(PlayerController player)
     {
